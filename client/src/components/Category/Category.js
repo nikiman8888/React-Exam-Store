a6bc3d033205690dbd8baac110f129981c687a23
Nikolay <nikiman88@yahoo.com>
2019-12-12 02:33:11 +0800

    fix bugses

--- client/src/components/Category/Category.js
+++ client/src/components/Category/Category.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './Category.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function Category(){
+function Category() {
     return (
         <React.Fragment>
-        <h2>Choose item type</h2>
-        <div className = "links-container">
-        <Link to = {{pathname:'/',senderInfo:'hats'}}>Santa Hats</Link>
-        <Link to = {{pathname:'/',senderInfo:'gloves'}}>Santa Gloves</Link>
-        <Link to = {{pathname:'/',senderInfo:'boots'}}>Santa Boots</Link>
-        <Link to = {{pathname:'/',senderInfo:'toys'}}>Santa Toys</Link>
-        </div>
-
-     </React.Fragment>
+            <h2>Choose item type</h2>
+            <div className="links-container">
+                <Link to={{ pathname: '/', senderInfo: 'hats' }}>Santa Hats</Link>
+                <Link to={{ pathname: '/', senderInfo: 'gloves' }}>Santa Gloves</Link>
+                <Link to={{ pathname: '/', senderInfo: 'boots' }}>Santa Boots</Link>
+                <Link to={{ pathname: '/', senderInfo: 'toys' }}>Santa Toys</Link>
+            </div>
+        </React.Fragment>
     )
 }
 
--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -1,49 +1,44 @@
 import React, { Component } from 'react';
-import './main.css';
+import './Main.css';
 import productService from '../../services/productService';
 import Products from '../products/Products';
-import NoProducts from '../NoProducst/NoProducts';
 
 class Main extends Component {
     constructor(props) {
         super(props)
         this.state = {
             products: [],
-            senderInfo:''
+            senderInfo: ''   // senderInfo e informaciata dali idva ot Category page
         }
     }
-    
+   
+
     componentDidMount() {
-        this.setState({senderInfo:this.props.location.senderInfo}) // vzemame info ot koia stranica sym prepraten
-              
-        productService.getAll() 
+        this.setState({ senderInfo: this.props.location.senderInfo }) // vzemame info ot koia stranica sym prepraten
+
+        productService.getAll()
             .then(res => res.json())
             .then(products => {
-                if(this.state.senderInfo!== undefined){   //akoi imame senderInfo zna4i idva ot Category
-                    
+                if (this.state.senderInfo !== undefined) {   //akoi imame senderInfo zna4i idva ot Category
+
                     let newProducts = products
-                    .filter(product =>product.category === this.state.senderInfo);
-                                     
-                    this.setState({products:newProducts});
-                    this.setState({senderInfo:this.props.location.senderInfo});
-                        
-                }else{
-                    this.setState({ products: products })   
-                }                                      
-            })
-            .then(()=>{
-                if(this.state.products.length === 0){
-                    this.props.history.push('/no-products')
+                        .filter(product => product.category === this.state.senderInfo);
+
+                    this.setState({ products: newProducts });
+                    this.setState({ senderInfo: this.props.location.senderInfo });
+                }
+                else {
+                    this.setState({ products: products })
                 }
             })
     }
-   
+
     render() {
         const page = 'main'
         return (
 
-            <div >              
-                    <h2>{this.state.senderInfo === undefined? 'All Products':this.state.senderInfo.toLocaleUpperCase()}</h2>
+            <div >
+                <h2>{this.state.senderInfo === undefined ? 'All Products' : this.state.senderInfo.toLocaleUpperCase()}</h2>
                 <Products products={this.state.products} page={page} />
             </div>
         )
