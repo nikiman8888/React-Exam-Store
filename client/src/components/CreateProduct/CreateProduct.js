f50d09735709f7660c08927afcbd1511c7838dfa
Nikolay <nikiman88@yahoo.com>
2019-12-13 08:09:36 +0800

    Create porductValidation

--- client/src/components/CreateProduct/CreateProduct.js
+++ client/src/components/CreateProduct/CreateProduct.js
@@ -1,111 +1,122 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './CreateProduct.css';
-import productServices  from '../../services/productService';
+import productServices from '../../services/productService';
+import productValidator from '../../formValidations/productValidator';
+import { ToastsContainer, ToastsStore, ToastsContainerPosition } from 'react-toasts';
 
-class CreateProduct extends Component{
-    constructor(props){
+class CreateProduct extends Component {
+    constructor(props) {
         super(props)
-        this.state ={
-            title:'',
-            price:'',
-            imageUrl:'',
+        this.state = {
+            title: '',
+            price: '',
+            imageUrl: '',
             category: 'hats', // zashtoto ako potrebitelia ne smeni options categoriata ostava prazna
             description: '',                          // i zatova go setvam predvariteelno
-            sales:0
+            sales: 0
         }
     }
-    counter = () =>{
+    counter = () => {
 
     }
-    changeHandlerTitle =(e)=>{
+    changeHandlerTitle = (e) => {
         this.setState({
-            title:e.target.value
-        }) 
-    }
-
-    handleChangeSelect = (e) =>{
-        this.setState({category:e.target.value});
-      }
-
-    changeHandlerDescription = (e) =>{
-        this.setState({
-            description:e.target.value
-            
+            title: e.target.value
         })
     }
 
-    changeHandlerPrice =(e)=>{
-        this.setState({
-            price:e.target.value
-        }) 
-    }
-    
-    changeHandlerImage =(e)=>{
-        this.setState({
-            imageUrl:e.target.value
-        }) 
+    handleChangeSelect = (e) => {
+        this.setState({ category: e.target.value });
     }
 
-    submitHandler =() =>{
-      
-        //tuk triabva validacii za poletatta
-     const data = this.state;
-     productServices.post(data)
-     .then(()=>{
-         this.props.history.push('/my-products')
-     })   
-     
-     //this.props.login(this.props.history,data);
-       
+    changeHandlerDescription = (e) => {
+        this.setState({
+            description: e.target.value
+
+        })
     }
-        
-    render (){
+
+    changeHandlerPrice = (e) => {
+        this.setState({
+            price: e.target.value
+        })
+    }
+
+    changeHandlerImage = (e) => {
+        this.setState({
+            imageUrl: e.target.value
+        })
+    }
+
+    submitHandler = () => {
+
+        let validator = productValidator(
+            this.state.title,
+            this.state.price,
+            this.state.imageUrl,
+            this.state.description
+        )
+        if (!validator) {
+            return;
+            //debugger;
+        }
+        const data = this.state;
+        productServices.post(data)
+            .then(() => {
+                this.props.history.push('/my-products')
+            })
+        //this.props.login(this.props.history,data);
+    }
+
+    render() {
         return (
-            
-            <div className = "shop-container">
-               <h2>Create Product</h2>
-               <form>
-                   <div className = "container-input">
-                      <label htmlFor = "title">Title</label>
-                      <input type ="text" placeholder = "title" name = "title" onChange = {this.changeHandlerTitle}/>
-                      
-                   </div>
+            <React.Fragment>
+                <ToastsContainer store={ToastsStore} position={ToastsContainerPosition.TOP_CENTER} />
+                <div className="shop-container">
+                    <h2>Create Product</h2>
+                    <form>
+                        <div className="container-input">
+                            <label htmlFor="title">Title</label>
+                            <input type="text" placeholder="title" name="title" onChange={this.changeHandlerTitle} />
 
-                   <div className = "container-input">
-                      <label htmlFor = "select">Select Category</label>
-                      <select type ="text"  name = "select" onChange = {this.handleChangeSelect} >
-                          
-                          <option value = "hats"  >Hats</option>
-                          <option value = "gloves">Gloves</option>
-                          <option value = "shoes">Shoes</option>
-                          <option value = "toys">Toys</option>
-                      </select>
-                   </div>
+                        </div>
 
-                   <div className = "container-input">
-                      <label htmlFor = "desription">Description</label>
-                      <textarea type ="text" placeholder = "max 50 symbols" name = "description"  onChange = {this.changeHandlerDescription}/>
-                   </div>
+                        <div className="container-input">
+                            <label htmlFor="select">Select Category</label>
+                            <select type="text" name="select" onChange={this.handleChangeSelect} >
 
-                   <div className = "container-input">
-                      <label htmlFor = "price">Price</label>
-                      <input type ="text" placeholder = "price" name = "price"  onChange = {this.changeHandlerPrice}/>
-                   </div>
+                                <option value="hats"  >Hats</option>
+                                <option value="gloves">Gloves</option>
+                                <option value="shoes">Shoes</option>
+                                <option value="toys">Toys</option>
+                            </select>
+                        </div>
 
-                  
-                   <div className = "container-input">
-                      <label htmlFor = "imageUrl">Photo</label>
-                      <input type ="text" placeholder = "put url of photo" name = "imageUrl"  onChange = {this.changeHandlerImage}/>
-                   </div>
-                  
-                   <div className = "container-input" >
-                      <button type = "button" onClick = {this.submitHandler}>Create</button>
-                   </div>
-               </form>
-            </div>
+                        <div className="container-input">
+                            <label htmlFor="desription">Description</label>
+                            <textarea type="text" placeholder="max 50 symbols" name="description" onChange={this.changeHandlerDescription} />
+                        </div>
+
+                        <div className="container-input">
+                            <label htmlFor="price">Price</label>
+                            <input type="text" placeholder="price" name="price" onChange={this.changeHandlerPrice} />
+                        </div>
+
+
+                        <div className="container-input">
+                            <label htmlFor="imageUrl">Photo</label>
+                            <input type="text" placeholder="put url of photo" name="imageUrl" onChange={this.changeHandlerImage} />
+                        </div>
+
+                        <div className="container-input" >
+                            <button type="button" onClick={this.submitHandler}>Create</button>
+                        </div>
+                    </form>
+                </div>
+            </React.Fragment>
+
         )
     }
-    
 }
 
 export default CreateProduct;
---
+++ client/src/formValidations/productValidator.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ToastsStore } from 'react-toasts';
+
+
+function registerValidator(title, price,imageUrl,description) {
+
+    if (title === '' || title.length < 2) {
+        ToastsStore.error('Title should be more than 2 charachter');
+        return false;
+    }
+    if (price === '' ||  +price === undefined || +price < 0) {
+        ToastsStore.error('Price have to be valid positve number ');
+        return false;
+    }
+
+    if (imageUrl === '') {
+        ToastsStore.error('Image Url should valid url');
+        return false;
+    }
+
+    if (description === '' || description.length < 5|| description.length >50 ) {
+        ToastsStore.error('Description must be between 5 and 50 charachters');
+        return false;
+    }
+    
+    return true;
+}
+export default registerValidator;
